82093287bd4c84d7262c9732580da98c97891b1b
Samuel Andrade <62983796+SamuelAndradesmn4@users.noreply.github.com>
2021-03-30 22:34:02 -0400

    Calculando o Dobro, Triplo e Raiz Quadrada de um numero inteiro
    Neste programa o User digita um número e o programa mostar na tela o Dobro Triplo e Raiz Quadrada do Número Digitado.

---
+++ ex006.py
@@ -0,0 +1,3 @@
+# Calculando o Dobro, Triplo e Raiz Quadrade de um numero inteiro
+n = int(input('Digite um numero:'))
+print('O dobro de {} é: {}, o triplo é:{}, e raiz quadrada é:{:.2f}'.format(n, (n*2), (n*3), n ** (1/2)))
