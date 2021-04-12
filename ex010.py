910d8e0b0aed1fe7ea7b4069c283bb01777671e6
Samuel Andrade <62983796+SamuelAndradesmn4@users.noreply.github.com>
2021-04-12 05:00:49 -0400

    Conversor de moedas
    Um program que le o quanto de dinheiro uma pessoa possui em reais e mostre quanos em dolár ela pode comprar.

---
+++ ex010.py
@@ -0,0 +1,6 @@
+# Conversor de moedas - Dolar em 30/03/2021 = 5,77
+n = float(input('Digite sua Quantidade de Dinheiro: R$'))
+dolar = n / 5.77
+print('Com R${} voce pode comprar US${:.2f}'.format(n, dolar))
+# Conversor de Dolar em Moeda Reais. Para dolar.
+
